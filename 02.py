aadfe686de2cf81411cbfff82baaaf0fdec91163
kinglongdai <kinglongdai@qq.com>
2021-09-28 14:04:31 +0800

    提交

---
+++ 02.py
@@ -0,0 +1,8 @@
+from _typeshed import IdentityFunction
+
+
+def func(day: int = 1):
+    return day+1
+
+def returnName():
+    pass;
---
+++ default.py
@@ -0,0 +1,19 @@
+import re
+import numpy as np
+import pandas as pd
+import time 
+
+
+# line = 'asdf fjdk; afed, fjek,asdf, foo'
+# line = tuple(line.split(";"))[0]
+# print(line)
+
+# print(np.abs(123))
+
+print(np.dtype('i4'))
+
+dt = np.dtype([('age',np.int8)]) 
+
+print(np.ndarray(123).ndim)
+
+a=1;
